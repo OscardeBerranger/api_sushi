750f92ccd088d554853e02194a2e1ed8f9905399
ravioli <lucj6778@gmail.com>
2023-10-16 13:00:10 +0200

    Api complète

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const pizzaRoutes = require('./route/pizza')
+const session = require('express-session')
+const mongodbSession = require('connect-mongodb-session')(session)
+const mongoUri = "mongodb://localhost:27017/sessions"
+
+const mongoose = require('mongoose')
+
+const store = new mongodbSession({
+    'uri': mongoUri,
+    collection: "mySessions",
+})
+
+app.use(
+    session({
+        secret: "This is the key",
+        resave: false,
+        saveUninitialized: false,
+        store : store
+    })
+)
+mongoose
+    .connect('mongodb://localhost:27017/foodtruck')
+    .then(()=>{
+        console.log('On est bien connectés')
+    })
+    .catch((err)=>{console.log(err)})
+
+
+app.get('/connect', (req, res)=>{
+    req.session.isAuth = true
+    console.log(req.session)
+    res.send('Connected')
+})
+
+
+app.use(express.json())
+app.use('/api/pizza', pizzaRoutes)
+
+
+app.listen(port, ()=>{
+    console.log('Hello World !')
+})
+
+
+
